3013eb800253aac9cae2996550bb3d812eb86c53
unknown <1246918397@qq.com>
2020-06-28 00:30:19 +0800

    新增投资人注册，绑卡操作

--- kingdom-dao/src/main/java/com/kingdom/dao/UserMapper.java
+++ kingdom-dao/src/main/java/com/kingdom/dao/UserMapper.java
@@ -23,7 +23,26 @@ public interface UserMapper {
 
 
     /**
-     * 投资人登陆功能，从数据库中查询密码
+     * 投资人注册功能，向数据库写进用户名，邮箱,手机号和密码
+     * @param userId
+     * @param userName
+     * @param email
+     * @param phoneNumber
+     * @param password
+     * @return int
+     */
+    int addUser(@Param("userid")Integer userId,@Param("username")String userName,@Param("email")String email,@Param("phonenumber")String phoneNumber,@Param("password")String password);
+
+    /**
+     * 投资人绑卡功能，将姓名和卡号写进数据库
+     * @param cardId
+     * @param realName
+     * @param cardNumber
+     */
+
+    int addCard(@Param("cardid") Integer cardId,@Param("realname") String realName,@Param("cardnumber") String cardNumber);
+    /**
+     * 投资人登录功能，从数据库中查询密码
      * @param email
      * @param phoneNumber
      * @return User
